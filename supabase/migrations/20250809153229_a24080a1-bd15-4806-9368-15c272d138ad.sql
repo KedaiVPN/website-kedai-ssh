334aa4915b3bec53b9f0d254810b7746a2c9899d
KedaiVPN <caracarayt0@gmail.com>
2025-08-19 17:38:02 +0700

    Delete supabase/migrations directory

--- supabase/migrations/20250809153229_a24080a1-bd15-4806-9368-15c272d138ad.sql
+++
@@ -1,6 +0,0 @@
-
--- Add role column to users table with default value 'member'
-ALTER TABLE users ADD COLUMN role TEXT DEFAULT 'member' CHECK (role IN ('member', 'reseller'));
-
--- Create index for better performance on role queries
-CREATE INDEX idx_users_role ON users(role);
